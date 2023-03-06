a1573643f7f9deb520f30a192d158b2669e67d52
Abbas <abbassarwar100@gmail.com>
2023-03-06 21:21:19 +0500

    bug removed by applying screen-width

--- js/menu.js
+++ js/menu.js
@@ -2,18 +2,23 @@ let menuIcon = document.querySelector('.menu-icon')
 let menuLinks = document.querySelector('.menu-links1')
 let btnClose = document.querySelector('.close')
 let menu_a = document.querySelectorAll('.menu-a')
-menuIcon.addEventListener('click', (e)=>{
-    if(menuLinks.style.display = 'none' || btnClose.style.display == 'none'){
-        menuLinks.style.display ='flex'
-        btnClose.style.display = 'block'
-    }else{
-         menuLinks.style.display = 'none'
-    }
-})
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+    menuIcon.addEventListener('click', (e)=>{
+        if(menuLinks.style.display = 'none' || btnClose.style.display == 'none'){
+            menuLinks.style.display ='flex'
+            btnClose.style.display = 'block'
+        }else{
+             menuLinks.style.display = 'none'
+        }
+    })
+  }
+
 btnClose.addEventListener('click', ()=>{
     if(menuLinks.style.display = 'flex'){
         menuLinks.style.display = 'none'
         btnClose.style.display = 'none'
+        location.reload()
     }
 })
 
