e3be6b659e80c2fa5f6836cc1b2ebef184068f2a
Abbas <abbassarwar100@gmail.com>
2023-03-07 10:35:02 +0500

    screen updated

--- js/menu.js
+++ js/menu.js
@@ -3,24 +3,24 @@ let menuLinks = document.querySelector('.menu-links1')
 let btnClose = document.querySelector('.close')
 let menu_a = document.querySelectorAll('.menu-a')
 
-if (window.matchMedia("(max-width: 768px)").matches) {
+if(window.innerWidth < 769){
     menuIcon.addEventListener('click', (e)=>{
         if(menuLinks.style.display = 'none' || btnClose.style.display == 'none'){
-            menuLinks.style.display ='flex'
-            btnClose.style.display = 'block'
+           menuLinks.style.display ='flex'
+           btnClose.style.display = 'block'
         }else{
-             menuLinks.style.display = 'none'
+           menuLinks.style.display = 'none'
         }
     })
-  }
-
-btnClose.addEventListener('click', ()=>{
-    if(menuLinks.style.display = 'flex'){
-        menuLinks.style.display = 'none'
-        btnClose.style.display = 'none'
-        location.reload()
-    }
-})
+   
+   btnClose.addEventListener('click', ()=>{
+       if(menuLinks.style.display = 'flex'){
+           menuLinks.style.display = 'none'
+           btnClose.style.display = 'none'
+           location.reload()
+       }
+   })
+}
 
 for(let i=0; i < menu_a.length; i++){
     menu_a[i].onclick = (e)=>{
