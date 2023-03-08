60964b811d9e85ead5955558ec743a8ed6211f38
Abbas <abbassarwar100@gmail.com>
2023-03-08 19:37:59 +0500

    Updated and fixed files

--- js/pop.js
+++ js/pop.js
@@ -1,48 +1,45 @@
-
-
-let btns = document.querySelectorAll('.btnx')
-let close = document.querySelector('.closeit')
-let container = document.querySelector('.work-main-container')
+const btns = document.querySelectorAll('.btnx');
+const close = document.querySelector('.closeit');
+const container = document.querySelector('.work-main-container');
 //
-let topic = document.querySelector('.topic-here')
-let description = document.querySelector('.paragh')
-let img = document.querySelector('.img-fluid')
+const topic = document.querySelector('.topic-here');
+const description = document.querySelector('.paragh');
+const img = document.querySelector('.img-fluid');
 //
-let objs = [
-    {
-        topic: "Tonic",
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-        img: 'image-resources/Snapshoot Portfolio3.jpg'
-    },
-    {
-        topic: "Multi Posting",
-        description: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-        img: 'image-resources/Snapshoot Portfolio2.jpg'
-    },
-    {
-        topic: "Facebook 360",
-        description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-        img: 'image-resources/Portfolio.png'
-    },
-    {
-        topic: "Uber Navigation",
-        description: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-        img: 'image-resources/Snapshoot Portfolio3.jpg'
+const objs = [
+  {
+    topic: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    img: 'image-resources/Snapshoot Portfolio3.jpg',
+  },
+  {
+    topic: 'Multi Posting',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    img: 'image-resources/Snapshoot Portfolio2.jpg',
+  },
+  {
+    topic: 'Facebook 360',
+    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    img: 'image-resources/Portfolio.png',
+  },
+  {
+    topic: 'Uber Navigation',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    img: 'image-resources/Snapshoot Portfolio3.jpg',
+  },
+];
+
+for (let i = 0; i < btns.length; i += 1) {
+  btns[i].addEventListener('click', () => {
+    container.classList.toggle('pop');
+    if (`btns${i}` === true) {
+      topic.innerHTML = objs[i].topic;
+      description.innerHTML = objs[i].description;
+      img.src = objs[i].img;
     }
-]
-
-for(let i=0; i < btns.length; i++){
-    btns[i].addEventListener('click', ()=>{
-        container.classList.toggle('pop')
-        let btnx = btns[i].classList
-        if(`btns${i}`){
-            topic.innerHTML = objs[i].topic
-            description.innerHTML = objs[i].description
-            img.src = objs[i].img
-        }
-    })
+  });
 }
 
-close.addEventListener('click', ()=>{
-    container.classList.remove('pop')
-})
+close.addEventListener('click', () => {
+  container.classList.remove('pop');
+});
--- js/validation.js
+++ js/validation.js
@@ -1,19 +1,17 @@
-let form = document.getElementById('form')
-let email = document.getElementById('mail')
-let error = document.querySelector('.error')
-form.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    function checkEmail() {
-        if (email.value.match(/[A-Z]/g)) {
-            error.innerHTML = "Email address should not contain capital letters.(Not Sent)";
-            event.preventDefault()
-        }else if(email.value.match(/[@]/g) == false){
-            error.innerHTML ='@ missing'
-            event.preventDefault()
-        } else {
-            error.innerHTML = ''
-            return true
-        }
-    }
-    checkEmail()
-})
+const form = document.getElementById('form');
+const email = document.getElementById('mail');
+const error = document.querySelector('.error');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (email.value.match(/[A-Z]/g)) {
+    error.innerHTML = 'Email address should not contain capital letters.(Not Sent)';
+    e.preventDefault();
+  } else if (email.value.match(/[@]/g) === false) {
+    error.innerHTML = '@ missing';
+    e.preventDefault();
+  } else {
+    error.innerHTML = '';
+    return true;
+  }
+  return error.innerHTML;
+});
